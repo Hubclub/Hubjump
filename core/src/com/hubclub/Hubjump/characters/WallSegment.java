5de6029d6c6799e006c6ecfe4154f6710dc4de0a
BalabanNeculai <balabanneculai920@gmail.com>
2014-08-11 21:50:47 +0300

    first step into generating the path

--- core/src/com/hubclub/Hubjump/characters/WallSegment.java
+++ core/src/com/hubclub/Hubjump/characters/WallSegment.java
@@ -1,5 +1,7 @@
 package com.hubclub.hubjump.characters;
 
+import java.util.Stack;
+
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -12,11 +14,14 @@ import com.hubclub.hubjump.worldenviroment.Enviroment;
 public class WallSegment {
 	public static final float WALL_WIDTH = 1.5f;
 	public static final float STAIR_WIDTH = 3f;
+	public static final float JUMP_HEIGHT = 2.9027f;
 	public static final String NAME = "WallSegment";
 	private static BodyDef wallDef;
 	//
-	private Body wallsegment;
 	static public Sprite wallSprite;
+	private static Vector2 lastJumpPoint;
+	private Stack<Vector2> jumpPoints;
+	private Body wallsegment;
 	
 	public void initializeBodyDef (){
 		wallDef = new BodyDef();
@@ -24,12 +29,35 @@ public class WallSegment {
 		wallDef.position.set(0, Enviroment.VP_HEIGHT);
 	}
 	
-	public WallSegment (){}
+	public WallSegment (){
+		jumpPoints= new Stack<Vector2>();
+	}
+	
+	private Vector2 getNextJumpPoint(Vector2 prevPoint){
+		// change the x to the center of the other wall
+		if (prevPoint.x == WALL_WIDTH/2)
+			prevPoint.x = Enviroment.VP_WIDTH - WALL_WIDTH/2;
+		else prevPoint.x= WALL_WIDTH/2;
+		
+		prevPoint.y+= JUMP_HEIGHT;
+		
+		//System.out.println(prevPoint);
+		return prevPoint;
+	}
 	
 	public void generateNextSegment (World world){
 			wallDef.position.y += Enviroment.VP_HEIGHT;
 		wallsegment = world.createBody(wallDef);
 		wallsegment.setUserData(this);
+		
+		//generate path (uses the previous generated point)
+		while (lastJumpPoint.y < wallDef.position.y){
+			jumpPoints.add(lastJumpPoint);
+			lastJumpPoint = getNextJumpPoint(lastJumpPoint);
+		}
+		
+		
+		
 		
 		PolygonShape wallShape = new PolygonShape();
 		wallShape.setAsBox(WALL_WIDTH/2 , Enviroment.VP_HEIGHT/2, new Vector2(WALL_WIDTH/2, -Enviroment.VP_HEIGHT/2) , 0);
@@ -41,6 +69,10 @@ public class WallSegment {
 	}
 	
 	public void generateFirstSegment (World world){
+		jumpPoints.add(new Vector2(WALL_WIDTH/2, Enviroment.VP_HEIGHT - Ninja.getNinjaHeight()/2) );
+		lastJumpPoint = getNextJumpPoint(jumpPoints.peek());
+
+		
 		initializeBodyDef();
 		wallsegment = world.createBody(wallDef);
 		wallsegment.setUserData(this);
@@ -55,6 +87,7 @@ public class WallSegment {
 		getWallsegment().createFixture(wallShape, 0);
 		
 		wallShape.dispose();
+
 	}
 	
 	public float getBottomBaseY (){
